afb34b5c176b5b04145aae96a2fff71d3ed9c92b
Marin Vilic <mavilic@fesb.hr>
2021-04-14 14:45:13 +0200

    Organization

---
+++ Entities/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public class Artist{
+    public string name { get; set; }
+    public string imagePath { get; set; }
+
+    public Artist(string passedName) {
+        name = passedName;
+    }
+
+    public Artist() { 
+    }
+}
+
